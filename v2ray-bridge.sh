c062a04d492ef456a21f8a2d714f016439b78e5a
Mohsen Fallahnejad <mohsenfallahnjd@gmail.com>
2023-02-02 01:41:30 +0330

    Enhance logs

--- v2ray-bridge.sh
+++ v2ray-bridge.sh
@@ -1,46 +1,58 @@
 #!/usr/bin/env bash
 
-function setShekan() {
+setShekan() {
     sed -i "s/nameserver/#nameserver/g" /etc/resolv.conf
     echo 'nameserver 178.22.122.100' >> /etc/resolv.conf
     echo 'nameserver 185.51.200.2' >> /etc/resolv.conf
 }
 
+sleepClear() {
+    sleep 3;
+    clear
+}
+
 while : ; do
-    read -p "Enter upstream ip:" upstreamIp
+    read -p "Enter upstream ip: " upstreamIp
     [[ "$upstreamIp" == "" ]] || break
 done
 
 while : ; do
-    read -p "Enter upstream uuid:" upstreamUUID
+    read -p "Enter upstream uuid: " upstreamUUID
     [[ "$upstreamUUID" == "" ]] || break
 done
 
-read -p "Enter plus number with port:" port
+read -p "Enter plus number with port: " port
 
 setShekan
 
 yes | sudo apt update
 
 setShekan
+sleepClear
 
 yes | sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
 setShekan
+sleepClear
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 setShekan
+sleepClear
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 setShekan
+sleepClear
 
 yes | sudo apt install docker-ce
 
 setShekan
+sleepClear
 
 yes | apt install docker-compose
+
+sleepClear
 
 cd ~ || exit
 
@@ -48,12 +60,23 @@ git clone https://github.com/miladrahimi/v2ray-docker-compose.git
 
 cd ~/v2ray-docker-compose/v2ray-bridge-server/ || exit
 
-yes | ../utils/bbr.sh
+yes "" | ../utils/bbr.sh
+
+echo ""
+echo ""
+echo "Set configs..."
+echo ""
+echo ""
 
 sed -i "s/<SHADOWSOCKS-PASSWORD>/Wom@NL!f3FR33DoM/" v2ray/config/config.json
 sed -i "s/<BRIDGE-UUID>/$(cat /proc/sys/kernel/random/uuid)/" v2ray/config/config.json
 sed -i "s/<UPSTREAM-IP>/$upstreamIp/" v2ray/config/config.json
 sed -i "s/<UPSTREAM-UUID>/$upstreamUUID/" v2ray/config/config.json
+
+echo ""
+echo "Configs added successfully."
+echo ""
+echo ""
 
 if [ "$port" != "" ]; then
     sed -i "s/1010/$(( 1010 + port ))/g" docker-compose.yml
@@ -65,6 +88,10 @@ if [ "$port" != "" ]; then
     sed -i "s/1110/$(( 1110 + port ))/g" v2ray/config/config.json
     sed -i "s/1210/$(( 1210 + port ))/g" v2ray/config/config.json
     sed -i "s/1310/$(( 1310 + port ))/g" v2ray/config/config.json
+
+    echo "Port Changed."
+    echo ""
+    echo ""
 fi
 
 cd ~/v2ray-docker-compose/v2ray-bridge-server/ || exit
@@ -72,7 +99,6 @@ cd ~/v2ray-docker-compose/v2ray-bridge-server/ || exit
 docker-compose up -d
 
 cd ~/v2ray-docker-compose/v2ray-bridge-server/ || exit
-
 
 echo ""
 echo ""
--- v2ray-upstream.sh
+++ v2ray-upstream.sh
@@ -1,18 +1,35 @@
 #!/usr/bin/env bash
 
+sleepClear() {
+    sleep 3;
+    clear
+}
+
 read -p "Enter port (default 1310):" port
 
 yes | sudo apt update
 
+sleepClear
+
 yes | sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+sleepClear
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
+sleepClear
+
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+sleepClear
 
 yes | sudo apt install docker-ce
 
+sleepClear
+
 yes | apt install docker-compose
+
+sleepClear
 
 cd ~ || exit
 
@@ -20,15 +37,38 @@ git clone https://github.com/miladrahimi/v2ray-docker-compose.git
 
 cd ~/v2ray-docker-compose/v2ray-upstream-server/ || exit
 
-yes | ../utils/bbr.sh
+yes "" | ../utils/bbr.sh
 
-sed -i "s/<UPSTREAM-UUID>/$(cat /proc/sys/kernel/random/uuid)/" v2ray/config/config.json
+uuid=$(cat /proc/sys/kernel/random/uuid)
+
+sed -i "s/<UPSTREAM-UUID>/$uuid/" v2ray/config/config.json
+
+echo ""
+echo ""
+echo "Uuid added successfully."
+echo ""
+echo ""
+
 
 if [ "$port" != "" ]; then
     sed -i "s/1310/$port/" v2ray/config/config.json
     sed -i "s/1310:1310/$port:$port/" docker-compose.yml
+
+    echo "Change port ..."
+    echo ""
+    echo ""
 fi
 
 cd ~/v2ray-docker-compose/v2ray-upstream-server/ || exit
 
 docker-compose up -d
+
+echo ""
+echo ""
+echo "================================================================"
+echo "Upstream uuid: $uuid"
+echo "Upstream ip: $(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')"
+echo "Upstream port: $port"
+echo "================================================================"
+echo ""
+echo ""
